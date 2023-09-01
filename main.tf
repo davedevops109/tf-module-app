738b43221f9e09895aaec0c86dbd34af7f0fe7a8
EPAPRA <dave.devops109@gmail.com>
2023-09-01 13:00:12 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -20,37 +20,7 @@ resource "aws_security_group" "main" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-docdb-subnet-group" }
+    { Name = "${var.env}-${var.component}-security-group" }
   )
 }
 
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "${var.env}-docdb-cluster"
-  engine                  = "docdb"
-  engine_version          = var.engine_version
-  master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
-  master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids  = [aws_security_group.docdb.id]
-  storage_encrypted = true
-  kms_key_id = data.aws_kms_key.key.arn
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-docdb-cluster" }
-  )
-}
-
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = var.number_of_instances
-  identifier         = "${var.env}-docdb-cluster-instance-${count.index+1}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = var.instance_class
-#  storage_encrypted = true
-#  kms_key_id = data.aws_kms_key.key.arn
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-cluster-instance-${count.index+1}" }
-  )
-}
